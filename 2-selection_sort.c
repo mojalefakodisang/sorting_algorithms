92d83debf06b86e8ed52dc6a34f284066f96f00d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2023-10-19 14:11:33 -0700

    [Update] - Made several changes

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,7 +14,7 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size <= 0)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
