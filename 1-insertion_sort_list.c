33aef8b5d5a15b7eddbcb28a9a1c8102f44335c7
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-23 10:04:37 +0200

    [Update] - Added one base case

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,7 +11,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *current, *key;
 	int temp;
 
-	if (!(*list) || !(*list)->next)
+	if (!list || !(*list) || !(*list)->next)
 		return;
 
 	current = (*list)->next;
