1bf061a05bd6283a00e8ae6f5cb0e7e86a817c3d
Mojalefa Kodisang <mojalefakodisang@yahoo.com>
2024-01-23 11:09:43 +0200

    [Update] - Added a condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -24,18 +24,22 @@ void swap(int *a, int *b)
 */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, min_idx;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 	
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[i] > array[j])
+			if (array[j] < array[min_idx])
+				min_idx = j;
+
+			if (min_idx != i)
 			{
-				swap(&array[i], &array[j]);
+				swap(&array[min_idx], &array[i]);
 				print_array(array, size);
 			}
 		}
